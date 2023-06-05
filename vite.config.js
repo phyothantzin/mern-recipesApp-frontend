01da4aada9f01c2e84db7ed254f944e500a39fc7
phyothantzin <105695346+phyothantzin@users.noreply.github.com>
2023-06-05 20:57:17 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://mernrecipeapp.onrender.com',
+        target: 'https://mernrecipeapp.onrender.com/',
         changeOrigin: true,
       },
     },

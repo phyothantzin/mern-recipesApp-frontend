f56d9113d9804e7b505d0f49f41ef3d2f546ab83
phyothantzin <105695346+phyothantzin@users.noreply.github.com>
2023-06-06 15:03:35 +0630

    Update usersApiSlice.js

--- src/slices/usersApiSlice.js
+++ src/slices/usersApiSlice.js
@@ -6,6 +6,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     login: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/login`,
+        credentials: "include",
         method: 'POST',
         body: data,
       }),
@@ -13,6 +14,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     register: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}`,
+        credentials: "include",
         method: 'POST',
         body: data,
       }),
